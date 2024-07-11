96b17d3796add6e6ed0777e9da9414d53892846b
azureem <91017820+azureem@users.noreply.github.com>
2024-07-11 15:47:06 +0500

    done

---
+++ app/src/main/java/com/example/eventapp/screen/info/InfoScreen.kt
@@ -0,0 +1,36 @@
+package com.example.eventapp.screen.info
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.eventapp.databinding.ScreenInfoBinding
+
+class InfoScreen : Fragment() {
+    private var _binding: ScreenInfoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = ScreenInfoBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().window.navigationBarColor = Color.parseColor("#2b2d42")
+        requireActivity().window.statusBarColor = Color.parseColor("#2b2d42")
+        binding.back.setOnClickListener { findNavController().navigateUp() }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
